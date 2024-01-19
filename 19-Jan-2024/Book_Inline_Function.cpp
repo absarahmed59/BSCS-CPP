d3026e723577b97c9194d5f3ed91d9749108c91c
Absar Ahmed <absarahmed59@outlook.com>
2024-01-19 16:05:22 +0500

    By Default Parameters

--- 19-Jan-2024/Book_Inline_Function.cpp
+++ 19-Jan-2024/Book_Inline_Function.cpp
@@ -5,6 +5,6 @@ inline void Book()  {   cout<<"C Plus Plus & Object Oriented BY IT SERIES"<<endl
 inline int Sum(int a, int b) {  return a+b; }
 int main(){
     Book();
-    cout<<Sum(10,20)<endl;
+    cout<<Sum(10,20)<<endl;
    return 0;
 }
